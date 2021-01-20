44155db68f28854f7e009bc3c067c9818f0c413f
Jose Silva <jose.silva@dcomp.ufs.br>
2021-01-19 23:59:29 -0300

    ajustes

--- monitor.py
+++ monitor.py
@@ -1,29 +1,28 @@
 import serial
 
 try:
-    arduino = serial.Serial("/dev/ttyACM0",timeout=1)
+	arduino = serial.Serial("/dev/ttyACM0",timeout=1)
 except:
-    print('Please check the port')
+	print('Please check the port')
 
-def receive_message():
-    
+def receive_message(msg):
+	print('jose')
 
 def clean(L):#L is a list
-    newl=[] #initialising the new list
-    for i in range(len(L)):
-        temp=L[i][2:]
-        newl.append(temp[:-5])
-    return newl
+	newl=[] #initialising the new list
+	for i in range(len(L)):
+		temp=L[i][2:]
+		newl.append(temp[:-5])
+	return newl
 
-"""Receiving data and storing it in a list"""
 while True:
-    rawdata=[]
-    rawdata.append(str(arduino.readline()))
-    msg=clean(rawdata)[0]
-    receive(msg)
+	rawdata=[]
+	rawdata.append(str(arduino.readline()))
+	msg = clean(rawdata)[0]
+	receive(msg)
 
 
-    
+	
 
 
 
