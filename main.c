695f0a452f24a65e7aaf28876111505b051a4e4d
Jose Silva <jose.silva@dcomp.ufs.br>
2021-02-01 23:51:28 -0300

    inicio uso de timers

--- main.c
+++ main.c
@@ -1,21 +1,60 @@
+#include <stdio.h>
+#include "comm/uart.h"
 #include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-float isPar(int num){
-
-  _delay_ms(2000);
-
-  if(num % 2 == 0){
-    return 1;
-  }else{
-    return 0; 
-  }
-
-  return -1;}
-
-int main(void) {
-    while(1) {       
-      int a = isPar(22);
-      _delay_ms(1000);     
+// global variable to count the number of overflows
+volatile int timer_overflow_count;
+volatile int timer_overflow_count_overflow;
+  
+// initialize timer, interrupt and variable
+void timer0_init(){
+    // set up timer with prescaler = 256
+    TCCR0B |= (1 << CS02);
+  
+    // initialize counter
+    TCNT0 = 0;
+  
+    // enable overflow interrupt
+    TIMSK0 |= (1 << TOIE0);
+  
+    // enable global interrupts
+    sei();
+  
+    // initialize overflow counter variable
+    timer_overflow_count = 0;
+    timer_overflow_count_overflow = 0;
+}
+  
+// TIMER0 overflow interrupt service routine
+// called whenever TCNT0 overflows
+ISR(TIMER0_OVF_vect){
+    // keep a track of number of overflows
+    timer_overflow_count++;
+    if (timer_overflow_count >= 65000)  // NOTE: '>=' is used
+    {  
+      timer_overflow_count_overflow++; 
+      TCNT0 = 0;            // reset counter
+      timer_overflow_count = 0;     // reset overflow counter
     }
-    return 0;
+}
+
+int main(void){
+  uart_init();
+  stdout = &uart_output;
+  
+  _delay_ms(2000);
+  puts("inicio");
+  puts("inicio");
+  puts("inicio");
+   // initialize timer
+  // timer0_init();
+
+  while(1) {    
+    printf("joseojsejosjejosejosjeoseosjeojseojsoejsoejosjeo\n");
+
+    // printf("overflow:%d:count_overflow:%d\n", timer_overflow_count, timer_overflow_count_overflow);
+  }
+  return 0;
 }
--- monitor.py
+++ monitor.py
@@ -1,11 +1,10 @@
 import serial
+import time
 
 try:
 	arduino = serial.Serial("/dev/ttyACM0",timeout=1)
 except:
 	print('Please check the port')
-
-
 
 def clean(L):#L is a list
 	newl=[] #initialising the new list
@@ -15,14 +14,33 @@ def clean(L):#L is a list
 	return newl
 
 def monitor():
+	iniCount = 0
+	tempoInicial = 0
+	last_overflow_counter = 0
+	tempoFinal = 0
+	iteracoes = 0
+	start = False
 	while True:
 		rawdata=[]
 		rawdata.append(str(arduino.readline()))
 		msg = clean(rawdata)[0]
-		if msg != '':
-			print(msg)
+		print(msg)		
 
-		
+		if msg == 'inicio':
+			iniCount += 1
+		if iniCount == 3 and not start:
+			print('[INFO] - Iniciando motiramento')
+			start = True	
+			tempoInicial = time.time()
+			continue
+
+		if start and msg != '' and msg != 'inicio':
+			overflow = int(msg.split(":")[1])
+			overflow_counter = int(msg.split(":")[3])
+			print('tempo decorrido ')
+			print(str(overflow_counter*65000*4.096/1000 + overflow*4.096/1000) + 's')
+	
+	
 monitor()
 
 	
