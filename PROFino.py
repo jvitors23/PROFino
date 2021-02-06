c587d9724e34cb98e7533b68547fe0a3bfa8aed4
Wesley Alves <wesley0775@gmail.com>
2021-02-06 19:43:28 -0300

    Adicionada validação da entrada via CLI. A porta agora deve ser informada explicitamente na chamada ao programa.

--- PROFino.py
+++ PROFino.py
@@ -1,10 +1,38 @@
 #!/usr/bin/python3
-import sys, subprocess
+import sys, getopt
 import instrument, make, monitor
 
+# Valida se os parâmetros foram passados corretamente pela linha de comando
+def evaluate_args():
+	short_options = 'hc:p:'
+	long_options = ['help', 'source=', 'port=']
+	mandatory_args = 2 # É obrigatório informar o '-c/--source' e o '-p/--port'
+
+	try:
+		args, values = getopt.getopt(sys.argv[1:], short_options, long_options)
+
+		if len(args) < mandatory_args:
+			args = [('-h', '')]
+
+		filename, port = '', ''
+		for arg, value in args:
+			if arg in ('-h', '--help'):
+				print ('Displaying help')
+				sys.exit(0) # Se por acaso o parâmetro de ajuda foi passado, não faz nada senão exibir a ajuda
+			elif arg in ('-c', '--source'):
+				filename = value
+			elif arg in ('-p', '--port'):
+				port = value
+
+		return filename, port
+	except getopt.error as e:
+		print (str(e))
+		sys.exit(2)
+
+# ...
 def main():
-	filename = sys.argv[1]
-	source = filename.split('.')[0] + '.inst'
+	filename, port = evaluate_args()
+	source = filename.split('.')[0] + '.inst' # "arquivo.inst.c"
 
 	functions = instrument.instrument(filename) # Instrumenta o código-fonte original
 	make.run(source) # Compila o código-fonte instrumentado e faz upload para o Arduino
