acf54c6b0d48888c94644b018918f1350d16fa57
Wesley Alves <wesley0775@gmail.com>
2021-02-06 17:49:59 -0300

    Extração de código repetido para a função 'make'

--- make.py
+++ make.py
@@ -1,26 +1,13 @@
 import os, subprocess
 
-# Executa um source especificado do Makefile
-def all(source):
-  compile(source)
-  upload(source)
-  clean(source)
+def run(source):
+  make(source, target='all')
+  make(source, target='program')
+  make(source, target='clean')
+  os.remove(source + '.c') # Apaga o código instrumentado
 
-
-def compile(source):
-  out = subprocess.Popen(['make', 'SOURCE=' + source], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+# Executa uma determinada regra ou a regra 'all', caso nada seja informado
+def make(source, target='all'):
+  out = subprocess.Popen(['make', target,'SOURCE=' + source], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
   stdout, stderr = out.communicate()
   print(stdout.decode("utf-8"))
-
-
-def upload(source):
-  out = subprocess.Popen(['make', 'program', 'SOURCE=' + source], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-  stdout, stderr = out.communicate()
-  print(stdout.decode("utf-8"))
-
-
-def clean(source):
-  out = subprocess.Popen(['make', 'clean', 'SOURCE=' + source], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-  stdout, stderr = out.communicate()
-  print(stdout.decode("utf-8"))
-  os.remove(source+'.c')
